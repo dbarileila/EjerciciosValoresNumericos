5d34995f7a0f3f09611279b53401ab0a598663fe
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-04-04 13:51:29 -0300

    ejercicio 16

---
+++ CAIGUIANUM_EJR16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIANUM_EJR16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Solicite un ingreso al usuario y determine si puede ser interpretado como un número entero.
+
+            bool bl = false;
+
+            do
+            {
+                Console.Write("Ingrese un valor numérico: ");
+                string ingreso = Console.ReadLine();
+
+                if (int.TryParse(ingreso, out int salida))
+                {
+                    Console.WriteLine($"Usted ingreso el número {ingreso} ");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Usted no ingreso un valor numérico.");
+                }
+
+            } while (bl == false);
+
+            Console.ReadKey();
+        }
+
+    }
+    }
+}
