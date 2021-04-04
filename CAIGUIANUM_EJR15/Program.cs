ad00c43c7a6b7df90a4cc8dd0435bd63099a8f09
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-04-04 13:34:52 -0300

    ejercicio 15

---
+++ CAIGUIANUM_EJR15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIANUM_EJR15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite un ingreso al usuario y determine si puede ser interpretado como un valor numérico.
+
+            bool bl = false;
+
+            do
+            {
+                Console.Write("Ingrese un valor numérico: ");
+                string ingreso = Console.ReadLine();
+
+                if (int.TryParse(ingreso, out int salida))
+                {
+                    Console.WriteLine($"Usted ingreso el número {ingreso} ");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Usted no ingreso un valor numérico.");
+                }
+
+            } while (bl == false);
+
+            Console.ReadKey();
+        }
+    }
+}
