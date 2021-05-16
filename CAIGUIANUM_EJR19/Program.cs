87a66a0c3f45f6ea9e66fe8b32f6c7f5ff204345
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-16 10:13:02 -0300

    ejercicio 19

---
+++ CAIGUIANUM_EJR19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIANUM_EJR19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite el ingreso de un número al usuario y presente la parte entera.
+            bool seguir = true;
+            while (seguir)
+            {
+                Console.Write("Ingrese un número decimal: ");
+                string ingreso = Console.ReadLine();
+
+                if (!double.TryParse(ingreso, out double salida))
+                {
+                    Console.WriteLine("Usted no ingreso un número decimal.");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Número ingresado: {salida}");
+                    Console.WriteLine($"Número redondeado: {Math.Round(salida)}");
+                    Console.WriteLine();
+                    Console.WriteLine("¿Desea ingresar otro número? (S/N)");
+                    var tecla = Console.ReadKey(intercept: true);
+                    seguir = tecla.Key == ConsoleKey.S;
+                }
+
+            }
+
+        }
+    }
+}
